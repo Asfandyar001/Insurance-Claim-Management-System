4154c64ca96ef91eea90b68ad8efcccb8dd058d1
Asfandyar001 <l227945@lhr.nu.edu.pk>
2025-08-01 20:43:10 +0500

    Initial Setup of files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold bg-amber-700 text-amber-950">
+        Hello World
+      </h1>
+      
+    </div>
+  );
+}
+
+export default App;
