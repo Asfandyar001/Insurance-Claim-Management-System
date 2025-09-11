26fb1ae5b09dc9c739db0399e7b068684416372a
Asfandyar001 <l227945@lhr.nu.edu.pk>
2025-09-12 04:14:28 +0500

    Few Initial Updates

--- frontend/src/pages/Active.jsx
+++ frontend/src/pages/Active.jsx
@@ -3,8 +3,13 @@ export default function Active() {
   return(
     <div>
       <div className="flex flex-col">
-        <h1 className="text-3xl font-semibold text-black dark:text-white">Active Claims Management</h1>
-        <p className="text-md text-gray-500">Manage and track insurance claims</p>
+        <h1 className="text-lg font-semibold text-black dark:text-white">Active Claims Management</h1>
+        <p className="text-sm text-gray-500">Manage and track insurance claims</p>
+      </div>
+
+      {/* This is the search,filters div */}
+      <div>
+
       </div>
     </div>
   )
