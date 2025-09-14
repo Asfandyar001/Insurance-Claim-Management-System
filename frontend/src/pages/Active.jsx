63d004d5a4baada18b1983637ea207bac485d393
Asfandyar001 <l227945@lhr.nu.edu.pk>
2025-09-14 05:57:31 +0500

    Fixed UI Issues related content moving around based on content length in Claims Cards

--- frontend/src/pages/Active.jsx
+++ frontend/src/pages/Active.jsx
@@ -214,42 +214,50 @@ export default function Active() {
                 <p className="text-sm text-gray-500">LCM: {claim.lcmRef}</p>
               </div>
 
-              <div className="flex fex-row items-center justify-start gap-36">
-                <div className="flex flex-row justify-start items-center gap-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
+              <div className="grid grid-cols-4 gap-8 items-center">
+                {/* Insured */}
+                <div className="flex flex-row items-center gap-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                   </svg>
-                  <div className="flex flex-col justify-center gap-0">
+                  <div className="flex flex-col">
                     <p className="text-sm font-medium dark:text-white">{claim.insured.name}</p>
                     <p className="text-sm text-gray-500">Insured</p>
                   </div>
                 </div>
 
-                <div className="flex flex-row justify-start items-center gap-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
+                {/* Loss Type */}
+                <div className="flex flex-row items-center gap-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                   </svg>
-                  <div className="flex flex-col justify-center gap-0">
+                  <div className="flex flex-col">
                     <p className="text-sm font-medium dark:text-white">{claim.lossType}</p>
                     <p className="text-sm text-gray-500">Loss Type</p>
                   </div>
                 </div>
 
-                <div className="flex flex-row justify-start items-center gap-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
+                {/* Loss Date */}
+                <div className="flex flex-row items-center gap-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5" />
                   </svg>
-                  <div className="flex flex-col justify-center gap-0">
+                  <div className="flex flex-col">
                     <p className="text-sm font-medium dark:text-white">{claim.dateOfLoss?.split("T")[0]}</p>
                     <p className="text-sm text-gray-500">Loss Date</p>
                   </div>
                 </div>
 
-                <div className="flex flex-row justify-start items-center gap-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
+                {/* Amount */}
+                <div className="flex flex-row items-center gap-3">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                    strokeWidth={2} stroke="currentColor" className="size-5 text-gray-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                   </svg>
-                  <div className="flex flex-col justify-center gap-0">
+                  <div className="flex flex-col">
                     <p className="text-sm font-medium dark:text-white">$12345678</p>
                     <p className="text-sm text-gray-500">Amount Made</p>
                   </div>
